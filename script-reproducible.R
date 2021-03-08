80cdf3461fc0db8fcdc30c88e6bcad5f62c3eab2
DarihanaLinaresL <darihanalinsres3@gmail.com>
2021-03-08 15:10:57 -0400

    construccion de Script reproducible

---
+++ script-reproducible.R
@@ -0,0 +1,93 @@
+
+# Intro a Grass, video no. 3
+# Parte reutilizable del script ----
+# Cargar paquetes
+library(rgrass7)
+gisdbase <- 'grass-data-test' #Base de datos de GRASS GIS
+wd <- getwd() #Directorio de trabajo
+wd
+loc <- initGRASS(gisBase = "/usr/lib/grass78/",
+                 home = wd,
+                 gisDbase = paste(wd, gisdbase, sep = '/'),
+                 location = 'guayubin',
+                 mapset = "PERMANENT",
+                 override = TRUE)
+# Fin de la parte reutilizable
+
+
+# Video no. 4, Definir proyección de la región de GRASS GIS, importar fuente y utilizarla para definir extensión y resolución. Cómo ver la ayuda de las funciones ----
+# Muestra la definición de la región
+gmeta()
+
+# Definir ruta del DEM
+dem <- 'datos-fuente/srtm_dem_cuenca_guayubin.tif'
+execGRASS(
+  cmd = 'g.proj',
+  flags = c('t','c'),
+  georef = dem)
+
+# Muestra la definición de la región modificada
+gmeta()
+
+#r.in.gdal importa la fuente a GRASS
+execGRASS(
+  cmd = 'r.in.gdal',
+  flags=c('overwrite','quiet'),
+  parameters=list(
+    input=dem,
+    output='dem'
+  )
+)
+
+# Actualizar la extensión de la región al DEM, sólo por precaución
+execGRASS(
+  cmd = 'g.region',
+  parameters=list(
+    raster = 'dem',
+    align = 'dem'
+  )
+)
+
+# Muestra la región de Grass again
+gmeta()
+
+# Importar vectorial a la región de Grass
+demext <- 'datos-fuente/srtm_dem_cuenca_guayubin.geojson'
+execGRASS(
+  cmd = 'v.in.ogr',
+  flags=c('overwrite','quiet'),
+  parameters=list(
+    input = demext,
+    output = 'dem_extent'
+  )
+)
+
+# Imprimir lista de mapas ráster y vectoriales dentro en la región/localización activa
+execGRASS(
+  'g.list',
+  flags = 't',
+  parameters = list(
+    type = c('raster', 'vector')
+  )
+)
+
+# Ver los addons disponibles en el repositorio oficial de GRASS GIS, incluyendo descripción
+execGRASS(
+  cmd = 'g.extension',
+  flags = 'c'
+)
+
+# Consultar la ayuda de una función
+parseGRASS("r.in.gdal")
+
+# Consultar la ayuda de una función. Segunda alternativa
+system('r.in.gdal --help')
+
+# Limpiar archivo de bloqueo del conjunto de mapas de GRASS
+unlink_.gislock()
+
+
+# Video no. 5, Explorar datos espaciales básicos entre GRASS y R ----
+# Imprimir lista de mapas ráster y vectoriales dentro en la región/localización activa
+
+
